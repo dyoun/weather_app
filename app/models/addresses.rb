0e363bcd7b9321b5953dcefa5a11d2c195852434
Dave Youn <dveyoun@gmail.com>
2025-08-17 10:09:18 -0700

    document address class

--- app/models/addresses.rb
+++ app/models/addresses.rb
@@ -1,4 +1,20 @@
 # model to validate and store address information
+#
+# usage:
+#   addresses.new(address: "123 main st", city: "seattle", state: "wa", zip: "98101").to_h
+#
+# attributes:
+# - address: string. street address
+# - city: string. city name
+# - state: string. state abbreviation
+# - zip: string. 5-digit zip code
+#
+# validations:
+# - address, city, state, zip must be present
+# - zip must be a 5-digit number
+#
+# methods:
+# - to_h: returns a hash of address attributes
 class Addresses
   include ActiveModel::Model
   include ActiveModel::Attributes
