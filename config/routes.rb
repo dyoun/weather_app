f27118c7d1fbfaeb4ecf093a264f9d92e7c4244e
Dave Youn <dveyoun@gmail.com>
2025-08-18 10:30:35 -0700

    add weather template

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Weather API endpoint
-  get "weather", to: "weather#show"
+  # weather endpoints
+  get "weather", to: "weather#index"
+  get "weather/api", to: "weather#show"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
