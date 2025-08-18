3d981acc364b0112d1daa3ba2b478b16fd0d7cc8
Dave Youn <dveyoun@gmail.com>
2025-08-18 10:29:45 -0700

    rescue weather errors

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -8,11 +8,27 @@
 #
 # methods:
 # - show: main action, renders weather data or error if address missing
-# - render_weather_by_address: fetches weather data using command
+# - get_weather_by_address: fetches weather data using command
 # - render_missing_parameters: renders error for missing address
 # - render_success_response: renders weather data as json
 class WeatherController < ApplicationController
   include ErrorHandler
+
+  def index
+    if params[:address].present?
+      @weather_data = get_weather_by_address
+    end
+  rescue Weather::Errors::ServiceUnavailableError => e
+    @error = "Weather service is currently unavailable: #{e.message}"
+  rescue Weather::Errors::InvalidResponseError => e
+    @error = "Unable to process weather data: #{e.message}"
+  rescue Weather::Errors::ApiError => e
+    @error = "Weather API error: #{e.message}"
+  rescue ArgumentError => e
+    @error = "Invalid request: #{e.message}"
+  rescue Weather::Errors::WeatherError => e
+    @error = "Weather error: #{e.message}"
+  end
 
   def show
     if params[:address].present?
@@ -20,8 +36,8 @@ class WeatherController < ApplicationController
     else
       render_missing_parameters
     end
-  rescue StandardError => e
-    render_error_response(e.message, :unprocessable_content)
+  rescue Weather::Errors::WeatherError => e
+    render_error_response("Error: #{e}", :bad_request)
   end
 
   private
@@ -30,6 +46,12 @@ class WeatherController < ApplicationController
     command = GetWeatherByAddressCommand.new(address: params[:address])
     weather_data = command.execute
     render_success_response(weather_data)
+  end
+
+  def get_weather_by_address
+    command = GetWeatherByAddressCommand.new(address: params[:address])
+    weather_data = command.execute
+    weather_data.to_h
   end
 
   def render_missing_parameters
